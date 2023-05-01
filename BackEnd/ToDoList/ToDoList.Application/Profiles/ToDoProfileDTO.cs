733c9ad64e82304c6e5c8b9f97fe2ceda3ea02bf
Esdras Rocha <ahoy.erocha@tcp.com.br>
2023-04-30 22:50:30 -0300

    Creating backend project

---
+++ BackEnd/ToDoList/ToDoList.Application/Profiles/ToDoProfileDTO.cs
@@ -0,0 +1,17 @@
+﻿using ToDoList.Application.Models.ToDo;
+using ToDoList.Application.Models;
+using AutoMapper;
+using ToDoList.Domain.Models;
+
+namespace ToDoList.Application.Profiles
+{
+    public class TodoProfileDTO : Profile
+    {
+        public TodoProfileDTO()
+        {
+            CreateMap<BaseEntity, BaseEntityDTO>().ReverseMap();
+            CreateMap<ToDo, ToDoDTO>().ReverseMap();
+            CreateMap<ToDo, CreateTodoDTO>().ReverseMap();
+        }
+    }
+}
